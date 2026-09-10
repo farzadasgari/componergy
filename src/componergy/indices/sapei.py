280c65989c2b56c241181e7c4597fbe21d900352
Farzad Asgari <khufarzadasgari@gmail.com>
2026-09-10 14:57:07 +0330

    add sapei water balance function

--- src/componergy/indices/sapei.py
+++ src/componergy/indices/sapei.py
@@ -11,3 +11,7 @@ from componergy.indices.loglogistic import fit_loglogistic_lmoments, loglogistic
 
 SAPEI_TIMESCALES_MONTHS = (3, 6, 9, 12)
 MIN_SAMPLES_PER_MONTH = 10
+
+def water_balance(monthly_ds: xr.Dataset) -> xr.DataArray:
+    """Monthly water balance (precipitation - PET), vectorized grid-wide."""
+    return monthly_ds["prcp"] - monthly_ds["pet"]
