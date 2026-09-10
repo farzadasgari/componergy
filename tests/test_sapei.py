9d0b8ab176cb273006a82b433fe0f5eed268b6a1
Farzad Asgari <khufarzadasgari@gmail.com>
2026-09-10 15:48:29 +0330

    add sapei test imports from componergy

--- tests/test_sapei.py
+++ tests/test_sapei.py
@@ -2,3 +2,11 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 from scipy.stats import norm
+
+from componergy.indices.sapei import (
+    add_sapei,
+    water_balance,
+    antecedent_water_balance,
+    SAPEI_TIMESCALES_MONTHS,
+)
+from componergy.indices.loglogistic import fit_loglogistic_lmoments, loglogistic_cdf
