cb1257822e5546f6eb3974685ba9c0c8da9b6dc4
Farzad Asgari <khufarzadasgari@gmail.com>
2026-09-10 14:41:04 +0330

    add sti climatology mean and std function

--- src/componergy/indices/sti.py
+++ src/componergy/indices/sti.py
@@ -1,3 +1,10 @@
 from __future__ import annotations
 
 import xarray as xr
+
+
+def climatology_mean_std(da: xr.DataArray) -> tuple[xr.DataArray, xr.DataArray]:
+    grouped = da.groupby("time.month")
+    mean = grouped.mean("time")
+    std = grouped.std("time")
+    return mean, std
