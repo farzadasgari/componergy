653330590cc0b766393287f7b5407d3f1ab0e59d
Farzad Asgari <khufarzadasgari@gmail.com>
2026-09-10 14:42:31 +0330

    add sti climatology sample size function

--- src/componergy/indices/sti.py
+++ src/componergy/indices/sti.py
@@ -8,3 +8,7 @@ def climatology_mean_std(da: xr.DataArray) -> tuple[xr.DataArray, xr.DataArray]:
     mean = grouped.mean("time")
     std = grouped.std("time")
     return mean, std
+
+
+def climatology_sample_size(da: xr.DataArray) -> xr.DataArray:
+    return da.groupby("time.month").count("time")
