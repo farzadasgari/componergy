42eaf4375bff2a3b02c0cce6c5f0b38efeb6c835
Farzad Asgari <khufarzadasgari@gmail.com>
2026-09-10 16:44:31 +0330

    add pipeline module imports

---
+++ src/componergy/indices/pipeline.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+import xarray as xr
+
+from componergy.indices.pet import add_pet
+from componergy.indices.sti import add_sti
+from componergy.indices.sapei import add_sapei
+from componergy.indices.scdhi import add_scdhi
