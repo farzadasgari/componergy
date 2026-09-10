829febe7b0c04e382e9e91f4a1833e651c9832a5
Farzad Asgari <khufarzadasgari@gmail.com>
2026-09-10 15:55:57 +0330

    add copulas clip helper

--- src/componergy/indices/copulas.py
+++ src/componergy/indices/copulas.py
@@ -5,3 +5,7 @@ from scipy.optimize import minimize_scalar
 from scipy.stats import kendalltau, kstest, multivariate_normal, norm
 
 EPS = 1e-6
+
+
+def _clip(u, v):
+    return np.clip(u, EPS, 1 - EPS), np.clip(v, EPS, 1 - EPS)
