192d7cc8b5136946b5de5d579011c1da4be0d2b2
Farzad Asgari <khufarzadasgari@gmail.com>
2026-09-10 15:53:25 +0330

    add sapei antecedent rolling sum test

--- tests/test_sapei.py
+++ tests/test_sapei.py
@@ -37,3 +37,15 @@ def test_water_balance_is_precip_minus_pet():
     wb = water_balance(ds)
     expected = ds["prcp"].values - ds["pet"].values
     assert np.allclose(wb.values, expected, equal_nan=True)
+
+
+def test_antecedent_water_balance_matches_manual_rolling_sum():
+    ds, times = _make_test_dataset(n_years=5, mask_one_cell=False)
+    wb = water_balance(ds)
+    wsd3 = antecedent_water_balance(wb, 3).isel(lat=0, lon=0).values
+    manual = wb.isel(lat=0, lon=0).values
+
+    assert np.isnan(wsd3[0]) and np.isnan(wsd3[1])
+    for i in range(2, len(manual)):
+        expected = manual[i - 2] + manual[i - 1] + manual[i]
+        assert abs(wsd3[i] - expected) < 1e-9
