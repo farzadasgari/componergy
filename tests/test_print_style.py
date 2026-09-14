f38f50b1c3bb05afbf4d5420f917703b2039b7b5
Farzad Asgari <khufarzadasgari@gmail.com>
2026-09-14 23:40:32 +0330

    add source hatch uniqueness test

--- tests/test_print_style.py
+++ tests/test_print_style.py
@@ -5,3 +5,8 @@ def test_no_two_sources_share_the_same_linestyle_and_marker_combination():
     combos = [(s["linestyle"], s["marker"]) for s in SOURCE_STYLE.values()]
     assert len(combos) == len(
         set(combos)), "two sources share a (linestyle, marker) pair -- would be indistinguishable in grayscale if colors also converge"
+
+
+def test_no_two_sources_share_the_same_hatch():
+    hatches = [s["hatch"] for s in SOURCE_STYLE.values()]
+    assert len(hatches) == len(set(hatches))
