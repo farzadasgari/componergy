bc0f10156f9943deb65cfcbd6a589c940065a4e9
Farzad Asgari <khufarzadasgari@gmail.com>
2026-09-14 21:17:00 +0330

    add source groups mapping

--- src/componergy/preprocessing/generation_eia.py
+++ src/componergy/preprocessing/generation_eia.py
@@ -1,3 +1,12 @@
 from __future__ import annotations
 
 import pandas as pd
+
+SOURCE_GROUPS = {
+    "Fossil": ["Coal", "Natural Gas", "Petroleum", "Other Gases"],
+    "Hydro": ["Hydroelectric Conventional"],
+    "Solar": ["Solar Thermal and Photovoltaic"],
+    "Wind": ["Wind"],
+    "Nuclear": ["Nuclear"],
+    "Other Renewable": ["Geothermal", "Wood and Wood Derived Fuels", "Other Biomass"],
+}
