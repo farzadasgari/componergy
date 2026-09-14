92aed5a9a4d1bb17dd02932eea738773ed2e4299
Farzad Asgari <khufarzadasgari@gmail.com>
2026-09-14 23:40:17 +0330

    add source style uniqueness test

--- tests/test_print_style.py
+++ tests/test_print_style.py
@@ -1,1 +1,7 @@
 from componergy.figures.print_style import SOURCE_STYLE, EVENT_STYLE
+
+
+def test_no_two_sources_share_the_same_linestyle_and_marker_combination():
+    combos = [(s["linestyle"], s["marker"]) for s in SOURCE_STYLE.values()]
+    assert len(combos) == len(
+        set(combos)), "two sources share a (linestyle, marker) pair -- would be indistinguishable in grayscale if colors also converge"
