415613f19324b967487f359939d43e910169b0fe
Farzad Asgari <khufarzadasgari@gmail.com>
2026-09-14 15:32:02 +0330

    add area weights function

--- src/componergy/figures/trend_stats.py
+++ src/componergy/figures/trend_stats.py
@@ -4,3 +4,8 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 from scipy import stats
+
+
+def compute_area_weights(lat: xr.DataArray) -> xr.DataArray:
+    weights = np.cos(np.deg2rad(lat))
+    return weights / weights.mean()
