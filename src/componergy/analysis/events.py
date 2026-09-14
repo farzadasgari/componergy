9fa9ea8dac83aa704f7adc8962cbbba58634e48e
Farzad Asgari <khufarzadasgari@gmail.com>
2026-09-14 14:44:01 +0330

    add classify compound function

--- src/componergy/analysis/events.py
+++ src/componergy/analysis/events.py
@@ -17,3 +17,8 @@ def classify_heatwave(sti: xr.DataArray, percentile: float = 0.90) -> xr.DataArr
 def classify_drought(sapei: xr.DataArray, percentile: float = 0.10) -> xr.DataArray:
     threshold = compute_percentile_threshold(sapei, percentile)
     return sapei < threshold
+
+
+def classify_compound(scdhi: xr.DataArray, percentile: float = 0.10) -> xr.DataArray:
+    threshold = compute_percentile_threshold(scdhi, percentile)
+    return scdhi < threshold
