89998b2918d801f31c049e7f088e4bef22870e68
Farzad Asgari <khufarzadasgari@gmail.com>
2026-09-14 23:36:54 +0330

    add source style dictionary

--- src/componergy/figures/print_style.py
+++ src/componergy/figures/print_style.py
@@ -1,1 +1,10 @@
 from __future__ import annotations
+
+SOURCE_STYLE = {
+    "Fossil": {"color": "#D55E00", "linestyle": "-", "marker": "o", "hatch": "//"},
+    "Hydro": {"color": "#0072B2", "linestyle": "--", "marker": "s", "hatch": "\\\\"},
+    "Solar": {"color": "#E69F00", "linestyle": (0, (3, 1, 1, 1)), "marker": "^", "hatch": "xx"},
+    "Wind": {"color": "#009E73", "linestyle": ":", "marker": "D", "hatch": ".."},
+    "Nuclear": {"color": "#CC79A7", "linestyle": "-.", "marker": "v", "hatch": "++"},
+    "Other Renewable": {"color": "#999999", "linestyle": (0, (5, 1)), "marker": "P", "hatch": "oo"},
+}
