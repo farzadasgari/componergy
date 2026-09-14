6166467944abb524bf2d68e0fbccb80cb858eac9
Farzad Asgari <khufarzadasgari@gmail.com>
2026-09-15 00:54:21 +0330

    add welch ttest function

--- src/componergy/figures/composite_stats.py
+++ src/componergy/figures/composite_stats.py
@@ -10,3 +10,12 @@ from scipy import stats
 
 def series_to_data_array(series: pd.Series) -> xr.DataArray:
     return xr.DataArray(series.values, dims="time", coords={"time": series.index})
+
+
+def welch_ttest(a: pd.Series, b: pd.Series) -> tuple[float, float]:
+    a = a.dropna()
+    b = b.dropna()
+    if len(a) < 5 or len(b) < 5:
+        return np.nan, np.nan
+    tstat, pval = stats.ttest_ind(a, b, equal_var=False)
+    return float(tstat), float(pval)
